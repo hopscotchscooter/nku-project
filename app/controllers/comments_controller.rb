987d14aaed45de2d37522c385c7138656f36a3b5
David <dvlewicki@gmail.com>
2014-04-16 21:59:29 +0000

    Fix username displaying for comments; Next step is to implement a rating system

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,25 @@ class CommentsController < ApplicationController
   
   def create
     @game = Game.find(params[:game_id])
+ 
+    @comment = @game.comments.build(comment_params)
     @comment = current_user.comments.build(comment_params)
-    @comment = @game.comments.create(comment_params)
+    @comment.author = current_user.username
     @comment.game_id = @game.id
+    @comment.user_id = current_user
     if @comment.save
-      redirect_to games_path(@game), notice: "Comment saved"
+      redirect_to game_path(@game), notice: "Comment saved"
     else
       render '/games'
     end
   end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to games_path, notice: "Comment deleted"
+  end
+  
     private
   def comment_params
     params.require(:comment).permit(:content)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416200752) do
+ActiveRecord::Schema.define(version: 20140416215022) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140416200752) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "game_id"
+    t.string   "author"
   end
 
   add_index "comments", ["game_id"], name: "index_comments_on_game_id"
